2341f88806246b33f9a1301bea15b748dd36dd3b
Kale Praveen Raj <kalepraveenraj@gmail.com>
2026-01-03 20:23:56 +0530

    Refactor PartsRepository to use MongoDB repository

--- backend/src/main/java/com/javaexpress/repository/PartsRepository.java
+++ backend/src/main/java/com/javaexpress/repository/PartsRepository.java
@@ -2,19 +2,15 @@ package com.javaexpress.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.javaexpress.entities.Category;
 import com.javaexpress.entities.Parts;
-@Repository
-public interface PartsRepository extends CrudRepository<Parts,String>{
-	//DSL-at run time queries it will generate automatically based on your method names
-	
-	//Optional<Category> findByIdAndBarCode(Integer id,String barCode);
-	
-	//Category findByNameOrderByIdAsc(String name);
 
-	Optional<Parts> findByPartNumber(String partNumber);
-	
+@Repository
+public interface PartsRepository extends MongoRepository<Parts, String> {
+
+    // MongoDB automatically creates query based on method name
+    Optional<Parts> findByPartNumber(String partNumber);
+
 }
